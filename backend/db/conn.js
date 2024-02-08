1644c0208dba05770db6071fd25cb6904dc2faa8
kashish432 <156075428+kashish432@users.noreply.github.com>
2024-02-08 18:18:14 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: "mysqlcontainer",
+  host: "34.131.83.76",
   user: "root",
   password: "kashish",
   port: "3306",
