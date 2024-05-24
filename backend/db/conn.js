045115636aa3babcacd8700e408237836452111c
kashish432 <156075428+kashish432@users.noreply.github.com>
2024-05-24 13:16:46 +0530

    Update conn.js

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -9,11 +9,19 @@ const connection = mysql.createConnection({
   port: "3306",
   database: "employees_db"
 });
-
-// open the MySQL connection
 connection.connect(error => {
-  if (error) throw error;
-  console.log("Successfully connected to the MYSQL database.");
+  if (error) {
+    console.error("Error connecting to MySQL:", error);
+    process.exit(1); // Exit the application if unable to connect
+  }
+  console.log("Successfully connected to the MySQL database.");
 });
 
 module.exports = connection;
+// open the MySQL connection
+// connection.connect(error => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the MYSQL database.");
+// });
+
+// module.exports = connection;
